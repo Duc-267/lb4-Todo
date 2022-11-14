d481163c4f6f2e89431e8f3cadcda27983a1831d
Duc-267 <mduc2672002@gmail.com>
2022-11-14 10:52:53 +0700

    Fix | Validate email

--- src/services/validator.service.ts
+++ src/services/validator.service.ts
@@ -1,8 +1,8 @@
-
 import { HttpErrors } from '@loopback/rest';
 import { Credentials, UserRepository } from '../repositories/user.repository';
 
 export async function validateCredentials(credentials: Credentials, userRepository: UserRepository) {
+  const regEx = /\S+@\S+\.\S+/;
   const foundUser = await userRepository.findOne({
     where: {
       email: credentials.email
@@ -17,4 +17,11 @@ export async function validateCredentials(credentials: Credentials, userReposito
   if (credentials.password.length < 8) {
     throw new HttpErrors.UnprocessableEntity("passwordd length should be greater than 8")
   }
+  if (!regEx.test(credentials.email)) {
+    throw new HttpErrors.UnprocessableEntity('email is not valid')
+  }
+}
+
+export async function validateEmail(email: string) {
+
 }
